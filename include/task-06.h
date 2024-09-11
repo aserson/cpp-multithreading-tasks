ac8e1655857925f22a093b6ecc7e8ec63561135a
aserson <zinyakov.timofey@gmail.com>
2024-09-11 18:58:28 +0400

    Task-06 About Multithread Vector Sort

---
+++ include/task-06.h
@@ -0,0 +1,91 @@
+﻿// Необходимо реализовать программу, которая выполняет сортировку большого массива чисел, используя 
+// многопоточность для повышения производительности. Массив должен быть разделён на несколько частей, 
+// каждая из которых сортируется в отдельном потоке. После сортировки всех частей, необходимо 
+// выполнить слияние (merge) отсортированных частей в один итоговый массив.
+
+#pragma once
+
+#include <thread>
+#include <vector>
+#include <algorithm>
+
+class MultiThreadSorter {
+    unsigned int thread_count;
+
+    std::vector<std::thread> tasks;
+public:
+    explicit MultiThreadSorter(const unsigned int thread_count)
+        : thread_count(thread_count) { }
+
+    ~MultiThreadSorter() {
+        joinAll();
+    }
+
+    std::vector<int> sort(std::vector<int> vec) {
+        const std::size_t vec_size = vec.size();
+
+        std::vector<int> output(vec_size);
+        std::size_t chunk_size = vec_size / thread_count;
+
+        for (int i = 0; i < thread_count; ++i) {
+            const std::size_t offset = i * chunk_size;
+            const std::size_t current_size = (i == thread_count - 1) ? (vec_size - offset) : chunk_size;
+
+            tasks.emplace_back(std::sort<std::vector<int>::iterator>, vec.begin() + offset, vec.begin() + offset + current_size);
+        }
+
+        joinAll();
+
+        for (int i = 0; i < thread_count; ++i) {
+            const std::size_t offset = i * chunk_size;
+            const std::size_t current_size = (i == thread_count - 1) ? (vec_size - offset) : chunk_size;
+
+            output = merge(output.begin(), output.begin() + offset, vec.begin() + offset, vec.begin() + offset + current_size);
+        }
+
+        return output;
+    }
+
+private:
+    std::vector<int> merge(std::vector<int>::iterator first1, std::vector<int>::iterator last1,
+        std::vector<int>::iterator first2, std::vector<int>::iterator last2) {
+
+        std::size_t size1 = last1 - first1;
+        std::size_t size2 = last2 - first2;
+
+        std::vector<int> output(size1 + size2);
+        auto iter = output.begin();
+
+        while (first1 != last1 || first2 != last2) {
+            if (first2 == last2) {
+                std::copy(first1, last1, iter);
+                return output;
+            }
+            else if (first1 == last1) {
+                std::copy(first2, last2, iter);
+                return output;
+            }
+
+            if (*first2 < *first1) {
+                *iter = *first2;
+                ++first2;
+                ++iter;
+            } else {
+                *iter = *first1;
+                ++first1;
+                ++iter;
+            }
+        }
+
+        return output;
+    }
+
+    void joinAll() {
+        for (auto& task : tasks) {
+            if (task.joinable()) {
+                task.join();
+            }
+        }
+        tasks.clear();
+    }
+};
---
+++ tests/test-06.cpp
@@ -0,0 +1,103 @@
+﻿#include "gtest/gtest.h"
+
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <cstring>
+#include <functional>
+#include <algorithm>
+
+#include "task-06.h"
+
+std::vector<int> CreateBuffer(const std::size_t buffer_size) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(-100, 100);
+
+    std::vector<int> output(buffer_size);
+    std::generate(output.begin(), output.end(), std::bind(dist, std::ref(gen)));
+
+    return output;
+}
+
+class Timer {
+public:
+    template<typename Func, typename... Args>
+    static auto measure(Func&& func, Args&&... args) {
+        const auto start = std::chrono::high_resolution_clock::now();
+        std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+        const auto end = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> duration = end - start;
+
+        return duration.count();
+    }
+};
+
+void SortingTest(std::vector<int> input, const std::size_t threads_count) {
+    std::cout << "Testing sorting results matching (buffer_length = " << input.size() << ", thread_count = " << threads_count << ")...";
+
+    std::vector<int> output;
+
+    {
+        MultiThreadSorter sorter(threads_count);
+        output = sorter.sort(input);
+    }
+
+    ASSERT_EQ(output.size(), input.size()) << "Sizes of input and output vectors are different";
+
+    std::sort(input.begin(), input.end());
+
+    for (int i = 0; i < output.size(); ++i)
+        ASSERT_EQ(output[i], input[i]) << "Mismatch at index " << i << " ("
+        << static_cast<int>(output[i]) << " vs "
+        << static_cast<int>(input[i]) << ")";
+
+    std::cout << " Done!" << std::endl;
+}
+
+TEST(Taks06, SmallArrayTest) {
+    SortingTest(CreateBuffer(10), 1);
+    SortingTest(CreateBuffer(10), 2);
+    SortingTest(CreateBuffer(10), 3);
+    SortingTest(CreateBuffer(10), 4);
+    SortingTest(CreateBuffer(10), std::thread::hardware_concurrency());
+}
+
+TEST(Taks06, LargeArrayTest) {
+    SortingTest(CreateBuffer(10000), 1);
+    SortingTest(CreateBuffer(10000), 2);
+    SortingTest(CreateBuffer(10000), 3);
+    SortingTest(CreateBuffer(10000), 4);
+    SortingTest(CreateBuffer(10000), std::thread::hardware_concurrency());
+}
+
+TEST(Taks06, AlreadySortedTest) {
+    auto data = CreateBuffer(500);
+    std::sort(data.begin(), data.end());
+
+    SortingTest(data, 1);
+    SortingTest(data, 2);
+    SortingTest(data, 3);
+    SortingTest(data, 4);
+    SortingTest(data, std::thread::hardware_concurrency());
+}
+
+TEST(Taks06, ReverseSortedTest) {
+    auto data = CreateBuffer(500);
+    std::sort(data.begin(), data.end(), [](int lha, int rha) { return lha > rha; });
+
+    SortingTest(data, 1);
+    SortingTest(data, 2);
+    SortingTest(data, 3);
+    SortingTest(data, 4);
+}
+
+TEST(Taks06, EmptyArrayTest) {
+    std::vector<int> data;
+
+    SortingTest(data, 1);
+    SortingTest(data, 2);
+    SortingTest(data, 3);
+    SortingTest(data, 4);
+    SortingTest(data, std::thread::hardware_concurrency());
+}
